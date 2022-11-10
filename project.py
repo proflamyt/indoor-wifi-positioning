3d1bee517f8a2c739ee1bb8c74a105082b10482e
proflamyt <53262578+proflamyt@users.noreply.github.com>
2022-11-10 15:19:24 +0100

    adding linux interface

--- project.py
+++ project.py
@@ -16,21 +16,21 @@ def get_os():
 
 # subprocess.Popen(['netsh', 'wlan', 'show', 'networks', 'mode=bssid']) ['netsh', 'wlan', 'show', 'interfaces'],
 
-def get_aps(platform):
+def get_aps(platform, interface='wlan'):
     if platform == 'win32':
-        scan_cmd = subprocess.Popen(['netsh', 'wlan', 'show', 'networks', 'mode=bssid'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        scan_cmd = subprocess.Popen(['netsh', interface, 'show', 'networks', 'mode=bssid'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         scan_out, scan_err = scan_cmd.communicate()
         scan_out_data = {}
         scan_out_lines = str(scan_out).split("\\n")[1:-1]
         for each_line in scan_out_lines:
             split_line = [e for e in each_line.split(" ") if e != ""]
             print(split_line)
-            print("==============")
+            
         #     line_data = {"SSID": split_line[0], "RSSI": int(split_line[2]), "channel": split_line[3], "HT": (split_line[4] == "Y"), "CC": split_line[5], "security": split_line[6]}
         #     scan_out_data[split_line[1]] = line_data
         # return scan_out_data
-    if platform == 'linux':
-        ...
+    if platform == 'linux' or platform == 'linux2':
+        scan_cmd  = subprocess.Popen(['iwlist', interface, 'scan'])
 
     if platform == 'mac':
         scan_cmd = subprocess.Popen(['airport', '-s'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
